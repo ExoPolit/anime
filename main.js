6fa8f3e58be6a07ef9312ee05d0d73203c1d2faf
ExoPolit <exopolit@gmx.de>
2023-11-15 11:42:06 +0100

    new update on JS

--- main.js
+++ main.js
@@ -23,7 +23,7 @@ function truncateSynopsis(synopsis) {
 if(!synopsis){
   return '';
 }
-  const sentences = synopsis.split('.').slice(0, 2);
+  const sentences = synopsis.split('.').slice(0, 5);
   return sentences.join('.') + '.';
 }
 function showFullSynopsis(synopsis) {
